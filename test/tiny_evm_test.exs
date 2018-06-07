7ef20e5763361d4aff3faa6f40bc20e19ce4aa67
Ayrat Badykov <ayratin555@gmail.com>
2018-06-07 21:26:51 +0300

    add test scaffold

--- test/tiny_evm_test.exs
+++ test/tiny_evm_test.exs
@@ -2,7 +2,19 @@ defmodule TinyEvmTest do
   use ExUnit.Case
   doctest TinyEvm
 
-  test "greets the world" do
-    assert TinyEvm.hello() == :world
+  @common_tests_path "./test/support/ethereum_common_tests/VMTests/"
+
+  test "runs common test" do
+    # test = read_test_file("vmArithmeticTest/sub4.json")
+
+    # TODO: assert test
+  end
+
+  defp read_test_file(file_name) do
+    {:ok, file} = File.open(@common_tests_path <> file_name)
+
+    file
+    |> IO.read(:all)
+    |> Poison.decode!()
   end
 end
